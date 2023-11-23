12c1b6b228d6e82003459717d3636aaa1a22a90f
Ricardo Oliveira <ricardooliveira.pl216@academico.ifsul.edu.br>
2023-11-23 12:38:34 -0300

    Continuação: Avaliação Objetivo 6

--- src/main/java/br/edu/ifsul/ctsi/lpoo_obj6/paciente/PacienteController.java
+++ src/main/java/br/edu/ifsul/ctsi/lpoo_obj6/paciente/PacienteController.java
@@ -2,10 +2,43 @@ package br.edu.ifsul.ctsi.lpoo_obj6.paciente;
 
 import org.springframework.stereotype.Controller;
 
+import java.util.Scanner;
+
 @Controller
 public class PacienteController {
-    public static void main(String[] args) {
+    private static final Scanner input = new Scanner(System.in);
+    private static PacienteService pacienteService;
 
+    public PacienteController(PacienteService pacienteService) {
+        PacienteController.pacienteService = pacienteService;
+    }
+    public static void main(String[] args) {
+        int opcao;
+        do {
+            System.out.print("\n\"-------  MENU PACIENTE -------\"");
+            System.out.print(
+                    """
+    
+                        1. Cadastrar paciente
+
+                        Opção (Zero p/sair):\s""");
+            opcao = input.nextInt();
+            input.nextLine();
+            switch (opcao) {
+                case 1 -> inserir();
+
+                default -> {
+                    if (opcao != 0) System.out.println("Opção inválida.");
+                }
+            }
+        } while (opcao != 0);
+    }
+    public static void inserir() {
+        Paciente pac = new Paciente();
+        System.out.println("\n++++++ Cadastro de novo Produto ++++++");
+        System.out.print("Digite o nome do produto: ");
+        pac.setNomePcnte(input.nextLine());
+        System.out.println("Produto salvo com sucesso:" + pacienteService.insert(pac));
     }
 }
 
--- src/main/java/br/edu/ifsul/ctsi/lpoo_obj6/paciente/PacienteService.java
+++ src/main/java/br/edu/ifsul/ctsi/lpoo_obj6/paciente/PacienteService.java
@@ -1,7 +1,16 @@
 package br.edu.ifsul.ctsi.lpoo_obj6.paciente;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 public class PacienteService {
+    @Autowired
+    private PacienteRepository rep;
+
+    public Paciente insert(Paciente paciente) {
+        Assert.isNull(paciente.getId(), "Erro! Não foi possível cadastrar.");
+        return rep.save(paciente);
+    }
 }
